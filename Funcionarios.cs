9b6b8b82c98a2d9756794c368aa30124306e590b
Andrea Del Picolo <109634520+adpicolo@users.noreply.github.com>
2022-11-18 11:27:04 -0300

    ListaFuncionário2

---
+++ Funcionarios.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaFuncionarios
+{
+    internal class Funcionarios
+    {
+        public int Id { get; set; }
+        public string Nome {get; set;}
+        public double Salario { get; set; }
+
+        public Funcionarios(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+        public void AumentoDeSalario(double porcentagem)
+        {
+            Salario += Salario * porcentagem / 100.0;
+        }
+
+
+        public override string ToString()
+        {
+            return Id + ", " + Nome + ", " + Salario.ToString("F2");
+            
+
+        }
+
+    }
+
+
+
+}
